20e08bdc5395e6d38fd00beb566ef36c81f33b3d
Ryo <21cm0108@jec.ac.jp>
2022-08-25 14:22:28 +0900

    Initial Commit

---
+++ didUpdateLocationsSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  didUpdateLocationsSample
+//
+//  Created by Ryo on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ didUpdateLocationsSample/didUpdateLocationsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  didUpdateLocationsSampleApp.swift
+//  didUpdateLocationsSample
+//
+//  Created by Ryo on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct didUpdateLocationsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
